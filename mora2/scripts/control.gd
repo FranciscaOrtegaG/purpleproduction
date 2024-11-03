11b2af4fedd20be2f88013d8290f9a581b7749de
FranciscaOrtegaG <71860064+FranciscaOrtegaG@users.noreply.github.com>
2024-11-03 16:11:11 -0300

    add: credits to tiles and enemy

--- mora2/scripts/control.gd
+++ mora2/scripts/control.gd
@@ -5,6 +5,7 @@ extends Control
 @onready var start: Button = %Start
 @onready var credits: Button = %Credits
 @onready var quit: Button = %Quit
+@onready var menu: Button = %Menu
 
 const CREDITS = preload("res://scenes/ui/creditos/Credits.tscn")
 
@@ -12,7 +13,9 @@ const CREDITS = preload("res://scenes/ui/creditos/Credits.tscn")
 func _ready() -> void:
 	start.pressed.connect(_on_start_pressed)
 	credits.pressed.connect(_on_credits_pressed)
+	#menu.pressed.connect(_on_menu_pressed)
 	quit.pressed.connect(get_tree().quit)
+	
 
 
 func _on_start_pressed() -> void:
@@ -21,6 +24,11 @@ func _on_start_pressed() -> void:
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_packed(CREDITS)
+	
+func _on_menu_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu/MainMenu.tscn")
+	
 	
 	
 	
