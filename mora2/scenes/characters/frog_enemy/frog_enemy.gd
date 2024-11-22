0d2c7cfb360f2926649e037ed1feb50d7703ce60
Jonathan R. <jonathandrp@outlook.es>
2024-11-22 11:27:50 -0300

    fixed frog dying

--- mora2/scenes/characters/frog_enemy/frog_enemy.gd
+++ mora2/scenes/characters/frog_enemy/frog_enemy.gd
@@ -7,12 +7,15 @@ extends Node2D
 @onready var hit_box: CollisionShape2D = $Area2D2/HitBox
 
 
+
 var hit_count = 0   # Cuenta las vidas restantes de la rana (inicialmente 0, sumaremos al recibir daño)
 var frog_lifes = 2
 var max_hits = 2    # Número de golpes necesarios para eliminar a la rana
+var dying = false
 
 func _ready() -> void:
-	animation_player.play("Idle")
+	if not dying:
+		animation_player.play("Idle")
 	# Conecta la señal de timeout del temporizador para que ejecute hop periódicamente
 	hop_timer.timeout.connect(_on_hop_timer_timeout)
 	
@@ -23,42 +26,51 @@ func _on_hop_finished(anim_name: String) -> void:
 		animation_player.animation_finished.disconnect(_on_hop_finished)
 
 func take_damage():
-	hit_count += 1
-	frog_lifes -= 1
-	animation_player.play("damage")
-	if hit_count >= max_hits:
-		die()
-	if frog_lifes == 0:
-		die()
+	if not dying:		
+		hit_count += 1
+		frog_lifes -= 1
+		animation_player.play("damage")
+		if hit_count >= max_hits:
+			die()
+		if frog_lifes == 0:
+			die()
 
 func die():
-	animation_player.play("explosion")
-	var finished_animation = await animation_player.animation_finished
-	if finished_animation == "explosion":
-		queue_free()
+	if not dying:
+		dying = true
+		animation_player.play("explosion")
+		var finished_animation = await animation_player.animation_finished
+		if finished_animation == "explosion":
+			print("explosion")
+			queue_free()
 
 func attack():
-	animation_player.play("attack")
+	if not dying:
+		animation_player.play("attack")
 	# Aquí puedes agregar lógica para el ataque si el jugador está cerca
 	
 
 func attack_player(body):
-	animation_player.play("attack")
-	body.take_damage(1, global_position)  # Asumiendo que el jugador tiene un método `take_damage(amount)`
+	if not dying:	
+		animation_player.play("attack")
+		body.take_damage(1, global_position)  # Asumiendo que el jugador tiene un método `take_damage(amount)`
 
 func _on_hop_timer_timeout() -> void:
-	# Reproducir animación de salto
-	animation_player.play("Hop")
-	# Conectar la señal para volver a "idle" después de la animación "hop"
-	animation_player.animation_finished.connect(_on_hop_finished)
+	if not dying:
+		# Reproducir animación de salto
+		animation_player.play("Hop")
+		# Conectar la señal para volver a "idle" después de la animación "hop"
+		animation_player.animation_finished.connect(_on_hop_finished)
 
 
 func _on_hit_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		attack_player(body)
+	if not dying:
+		if body.is_in_group("Player"):
+			attack_player(body)
 
 
 func _on_hurt_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		take_damage()
+		if not dying:	
+			take_damage()
 		body.bounce()
